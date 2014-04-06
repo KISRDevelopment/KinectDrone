6b7a6d1cb3dd202a39f520391021cc87cdbc1fac
Octowl <alsaff1987@gmail.com>
2014-04-06 09:41:39 +0300

    code for testing added

--- drone_control.py
+++ drone_control.py
@@ -1,19 +1,19 @@
 import python_ardrone.libardrone as libardrone
 
-drone = libardrone.ARDrone()
+#drone = libardrone.ARDrone()
 
 def main():
-    drone = libardrone.ARDrone()
-    drone.takeoff()
+#    drone = libardrone.ARDrone()
+#    drone.takeoff()
     running = True
     while running:
 	"""Set to hover between instructions"""
-	drone.hover()
+#	drone.hover()
 
 	#****************************
 	# TESTING INPUT
 	#****************************
-	RAW = input("<roll pitch throttle yaw quit>: ").split(" ")
+	RAW = [float(x) for x in raw_input("<roll pitch throttle yaw quit>: ").split(" ")]
 	
 	
         """Set the pitch, roll, and yaw"""
@@ -27,10 +27,15 @@ def main():
 
 	
 	"""Set the ControlVector"""
-	drone.controlVector = RAW[:-1]#[roll, pitch, throttle, yaw]
-	drone.vector_move()
+	print(RAW)#
+
+#	drone.controlVector = RAW[:-1]#[roll, pitch, throttle, yaw]
+#	drone.vector_move()
 
 	## TODO Add condition for quiting
 	if RAW[-1]: #quit:
-	    drone.land()
+	   # drone.land()
 	    running = False
+
+if __name__ == '__main__':
+	main()
