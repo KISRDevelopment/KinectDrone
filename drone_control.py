e63d66aa20cb115280522818dd98cfe2c6668858
Octowl <alsaff1987@gmail.com>
2014-04-06 09:26:14 +0300

    modified drone_control.py for testing

--- drone_control.py
+++ drone_control.py
@@ -9,20 +9,28 @@ def main():
     while running:
 	"""Set to hover between instructions"""
 	drone.hover()
+
+	#****************************
+	# TESTING INPUT
+	#****************************
+	RAW = input("<roll pitch throttle yaw quit>: ").split(" ")
+	
+	
         """Set the pitch, roll, and yaw"""
-	## TODO
+	## TODO this is the point where the kinect and the drone meet
 	pitch = 0
 	roll = 0
 	yaw = 0
+	throttle = 0
 	"""Set quitting signal"""
 	quit = 0
 
 	
 	"""Set the ControlVector"""
-	drone.controlVector = [roll, pitch, 0, yaw]
+	drone.controlVector = RAW[:-1]#[roll, pitch, throttle, yaw]
 	drone.vector_move()
 
 	## TODO Add condition for quiting
-	# if quit:
-	#    drone.land()
-	#    running = False
+	if RAW[-1]: #quit:
+	    drone.land()
+	    running = False
