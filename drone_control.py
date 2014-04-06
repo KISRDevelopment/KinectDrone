0532269f773ecf3a12e69de18ef571e1bdf9f444
Octowl <alsaff1987@gmail.com>
2014-04-06 10:25:30 +0300

    testing manual control through command line

--- drone_control.py
+++ drone_control.py
@@ -1,14 +1,12 @@
 import python_ardrone.libardrone as libardrone
 
-#drone = libardrone.ARDrone()
-
 def main():
-#    drone = libardrone.ARDrone()
-#    drone.takeoff()
+    drone = libardrone.ARDrone()
+    drone.takeoff()
     running = True
     while running:
 	"""Set to hover between instructions"""
-#	drone.hover()
+	drone.hover()
 
 	#****************************
 	# TESTING INPUT
@@ -29,12 +27,11 @@ def main():
 	"""Set the ControlVector"""
 	print(RAW)#
 
-#	drone.controlVector = RAW[:-1]#[roll, pitch, throttle, yaw]
-#	drone.vector_move()
+	drone.controlVector = RAW[:-1]#[roll, pitch, throttle, yaw]
+	drone.vector_move()
 
-	## TODO Add condition for quiting
 	if RAW[-1]: #quit:
-	   # drone.land()
+	    drone.land()
 	    running = False
 
 if __name__ == '__main__':
