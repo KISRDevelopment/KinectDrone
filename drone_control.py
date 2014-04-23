5019cd10a9b713d6c796aebacb950ff2d78dd27f
Abdulrahman Alotaibi <Adminq80@gmail.com>
2014-04-23 18:46:44 +0300

    adding socket code to the mix

--- drone_control.py
+++ drone_control.py
@@ -1,30 +1,27 @@
 import python_ardrone.libardrone as libardrone
+#from kinect_socket import Kinect_sock
 
 def main():
     drone = libardrone.ARDrone()
     drone.takeoff()
-    running = True
     while 1: # running:
         try:
             """Set to hover between instructions"""
-    	    drone.hover()
+            drone.hover()
             """Set the pitch, roll, and yaw"""
-	    ## TODO
+            ## TODO
     	    pitch = 0
     	    roll = 0
-	    yaw = 0
-	    """Set quitting signal"""
-	    quit = 0
-
-
-	    """Set the ControlVector"""
-	    drone.controlVector = [roll, pitch, 0, yaw]
-	    drone.vector_move()
+            yaw = 0
+            drone.controlVector = [roll, pitch, 0, yaw]
+            drone.vector_move()
 
         except KeyboardInterrupt:
             drone.land()
-            raise
+            break
 	## TODO Add condition for quiting
 	# if quit:
 	#    drone.land()
 	#    running = False
+if __name__ == '__main__':
+    main()
